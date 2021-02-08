bd6ff9b05b61184a1ffab52bfb9e304ada7da72e
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-08 22:39:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  famous_cats = ["lil' bub", "grumpy cat", "Maru"]
-  famous_cats.push("nala cat")
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
