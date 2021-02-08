19518f2e1c3f184788d7c79db9bc85a158bdf8e7
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-08 22:38:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  puppies = ["bulldog", "terrier", "poodle"]
+  puppies = ["bulldog", "terrier", "poodle", "doggie"]
 end
 
 def add_element_to_end_of_array(array, element)
